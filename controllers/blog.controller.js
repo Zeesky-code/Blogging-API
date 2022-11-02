91588ec2f80007a0d7845da5278745fb7301f8be
Zainab Lawal <zainablaw1012@gmail.com>
2022-11-02 12:45:45 +0300

    Added JWT aut requirement to create blog route

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -5,10 +5,24 @@ require('dotenv').config();
 const Blog  = require('../models/blog.model')
 const User =  require('../models/user.model')
 
+const getTokenFrom =  req =>{
+    const authorization = req.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer')){
+        return authorization.substring(7)
+    }
+    return null
+}
+
 async function createBlog(req,res, next){
     const content = req.body
 
-    const user = await User.findById(content.userId)
+    const token = getTokenFrom(req)
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+    if (!decodedToken){
+        return res.status.json({error: 'Invalid or Missing Token'})
+    }
+    const user = await User.findById(decodedToken.user._id)
+
 
     const blog = new Blog({
         title: content.title,
@@ -16,15 +30,22 @@ async function createBlog(req,res, next){
         body: content.body,
         author: user._id
     })
+    try{
+        const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+        res.status(201).json({
+            message: "Blog saved successfully",
+            savedBlog
+        })
+    }catch{
+        res.status(400).json({
+            "error": "Blog Titles must be unique"
+        })
+    }
+    
 
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(newBlog._id)
-    await user.save()
-
-    res.status(201).json({
-        message: "Blog saved successfully",
-        savedBlog
-    })
+    
 }
 
 module.exports = {
