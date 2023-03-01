80bbf1ef1db2bcb4a656392ef6e0a64000187391
Zainab Lawal <zainablaw1012@gmail.com>
2023-03-01 23:57:53 +0300

    Integrated swagger and added blog schema

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const passport = require('passport')
 const rateLimit = require('express-rate-limit')
+const swaggerJsdoc = require("swagger-jsdoc")
+const swaggerUi = require("swagger-ui-express")
 
 require('dotenv').config()
 
@@ -16,34 +18,63 @@ const commentRoute = require('./routes/comment.route')
 const app = express()
 
 //set up db connection
-const {connectToMongoDB} = require('./config/db')
+const { connectToMongoDB } = require('./config/db')
 connectToMongoDB()
 
 //set up rate limiting
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
 // Apply the rate limiting middleware to all requests
 app.use(limiter)
 
+//defining swagger options
+const options = {
+    definition: {
+        openapi: "3.1.0",
+        swagger: "2.0",
+        info: {
+            title: "Blogging API",
+            version: "1.0.0",
+            description: "An API for a blog built with Node.js, ExpressJS and MongoDB",
+            contact: {
+                name: "Zainab Lawal",
+                email: "zainablaw1012@gmail.com"
+            }
+
+        },
+        servers: [
+            {
+                url: "http://localhost:9000",
+                liveurl: "https://wild-pink-sturgeon-cape.cyclic.app/"
+            },
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
+
+
 app.use(passport.initialize());
-
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended:false}))
-
-
-
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
 app.use('/user', userRoute)
 app.use('/blog', blogRoute)
 app.use('/comments', commentRoute)
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.status(200).json({
         status: "true",
         message: "Welcome to Blogging API"
--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,3 +1,59 @@
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Blog:
+ *          type: object
+ *          required:
+ *              - title
+ *              - description
+ *              - body
+ *          properties:
+ *              id: 
+ *                  type: string
+ *                  description: The auto-generated id of the blog post
+ *              title:
+ *                  type: string
+ *                  description: The title of your blog post
+ *              body:
+ *                  type: string
+ *                  description: The body of your blog post
+ *              tags:
+ *                  type: string
+ *                  description: The tags of the blog post
+ *              comments:
+ *                  type: string
+ *                  description: The comments about the blog post
+ *              timestamp:
+ *                  type: string
+ *                  format: date
+ *                  description: The date the blog post was created
+ *              state:
+ *                  type: string
+ *                  description: Whether the blog post is publish or a draft. It defaults to draft.
+ *              author:
+ *                  type: string
+ *                  description: The id of the blog post's author
+ *              reading_time:
+ *                  type: number
+ *                  description: The number of time, in minutes it takes to read the blog post
+ *              read_count:
+ *                  type: number
+ *                  description: The number of times a blog post has been read
+ *          example:
+ *              _id: 6364aa4790f95c704c7444d2
+ *              title: Rollercoasters -  the hidden secret
+ *              description: A short story about the invention of rollercoasters and their tricks
+ *              body: This is a very short api. There are few endpoints and it's easy to lea…"
+ *              tags: Weather
+ *              timestamp: 2022-11-04T05:59:25.446+00:00
+ *              state: draft
+ *              author: 6364a4b6e85042c3c585724c
+ *              reading_time: 1
+ *              read_count: 4
+ * 
+ */
+
 const express = require('express');
 const blogRouter = express.Router()
 
